50aa3f2e7e7c6f7235a66c4d80c806e135d3be74
torbjornjohannsen <toej@itu.dk>
2024-09-19 10:52:05 +0200

    message printing

--- mandatory2/assign2.go
+++ mandatory2/assign2.go
@@ -17,6 +17,10 @@ type Message struct {
 	data           *rune
 }
 
+func (msg *Message) print() string {
+	return fmt.Sprintf("Seq: %d\nAck: %d\nFlags:\tSyn\tAck\tFin\n \t%t\t%t\t%t\nWindow_Size %d\nSegment_size %d\n", msg.sequence_num, msg.acknowledgment, msg.syn, msg.ack, msg.fin, msg.window_size, msg.segment_size)
+}
+
 func client(channel chan Message) {
 	msg := Message{rand.Int(), 0, true, false, false, 0, 0, nil}
 	channel <- msg
@@ -25,7 +29,7 @@ func client(channel chan Message) {
 		fmt.Println("Wrong message")
 		return
 	}
-	fmt.Printf("syn %d ack %d\n", msg2.sequence_num, msg2.acknowledgment)
+	fmt.Printf("Client recieved: %s\n", msg2.print())
 	msg3 := Message{msg2.acknowledgment, msg2.sequence_num + 1, false, true, false, 0, 0, nil}
 	channel <- msg3
 
@@ -37,7 +41,7 @@ func server(channel chan Message) {
 		fmt.Println("Wrong message")
 		return
 	}
-	fmt.Printf("syn %d\n", msg.sequence_num)
+	fmt.Printf("Server Recieved: %s\n", msg.print())
 	msg2 := Message{rand.Int(), msg.sequence_num + 1, true, true, false, 0, 0, nil}
 	channel <- msg2
 	msg3 := <-channel
@@ -45,7 +49,7 @@ func server(channel chan Message) {
 		fmt.Println("Wrong message")
 		return
 	}
-	fmt.Printf("ack seq %d acknum %d\n", msg3.sequence_num, msg3.acknowledgment)
+	fmt.Printf("Server recieved: %s\n", msg.print())
 
 }
 
