7bc41c54bbd71a8b2940b367f9fc97aeddbd6355
torbjornjohannsen <toej@itu.dk>
2024-09-19 13:18:04 +0200

    works initially

--- mandatory2/assign2.go
+++ mandatory2/assign2.go
@@ -71,46 +71,53 @@ func tls_server(inChannel chan Message, outChannel chan Message, timeout int) bo
 		return false
 	}
 
-	fmt.Printf("Server Recieved: %s\nTLS handshake done", msg3.print())
+	fmt.Printf("Server Recieved: %s\nTLS handshake done\n", msg3.print())
 	return true
 }
 
-func send(outChannel chan Message, inChannel chan Message, data []rune) {
+func send(inChannel chan Message, outChannel chan Message, data []rune) {
 	//
-	/* for !tls_server(outChannel, 1000) {
+	for !tls_server(outChannel, inChannel, 1000) {
 		time.Sleep(time.Duration(500) * time.Millisecond)
-	} */
+	}
 
 	var requestMsg Message
 	for !getMessageWithTimeout(&requestMsg, inChannel, 5000) {
+		fmt.Println("No req msg")
 	}
+
+	fmt.Printf("Got request msg: %s\n", requestMsg.print())
 
 	windowSize := requestMsg.window_size
 	segmentSize := requestMsg.segment_size
+	totalSegments := len(data) / segmentSize
+	if len(data)%segmentSize > 0 {
+		totalSegments++
+	}
 
 	indice := 0
 	counter := 0
-	segmentRecieved := make([]bool, len(data)/segmentSize+2) // +2 to avoid off by one error from acknowledgements being incremented automatically
+	segmentRecieved := make([]bool, totalSegments+2) // +2 to avoid off by one error from acknowledgements being incremented automatically
 
 	go func(segArr []bool, msgCh chan Message) {
 		for {
 			var ackMsg Message
-			if getMessageWithTimeout(&ackMsg, msgCh, 1000) {
-				segArr[ackMsg.acknowledgment] = true
+			if getMessageWithTimeout(&ackMsg, msgCh, 5000) {
+				segArr[ackMsg.acknowledgment-1] = true
 			}
 		}
 	}(segmentRecieved, inChannel)
 
-	for !segmentRecieved[len(data)/segmentSize+1] {
-		if !segmentRecieved[min(0, counter-windowSize)] { // if we have fired off the whole window-size without acknowledgement
-			counter = min(0, counter-windowSize) // reset to start of block
+	for !segmentRecieved[totalSegments-1] {
+		if !segmentRecieved[max(0, counter-windowSize)] { // if we have fired off the whole window-size without acknowledgement
+			counter = max(0, counter-windowSize) // reset to start of block
 			indice = segmentSize * counter
 		}
 
-		block := data[indice : indice+segmentSize]
-		indice += segmentSize
+		block := data[indice:min(indice+segmentSize, len(data))]
+		indice = min(indice+segmentSize, len(data))
 		msgBlock := Message{counter, 0, true, false, false, windowSize, segmentSize, block}
-
+		fmt.Printf("Server sent msg %d\n", counter)
 		outChannel <- msgBlock
 		counter++
 	}
@@ -134,6 +141,7 @@ func receive(inChannel chan Message, outChannel chan Message, timeout int) {
 	var ack = 1
 	for getMessageWithTimeout(&msg, inChannel, timeout) && !msg.fin {
 		outChannel <- Message{msg.sequence_num, ack, msg.syn, msg.ack, msg.fin, msg.window_size, msg.segment_size, msg.data}
+		fmt.Printf("Client got msg %d\n", msg.sequence_num)
 		if msg.sequence_num+1 == ack {
 			ack++
 		}
@@ -141,10 +149,11 @@ func receive(inChannel chan Message, outChannel chan Message, timeout int) {
 }
 
 func main() {
+
 	inChannel := make(chan Message)
 	outChannel := make(chan Message)
-	go tls_client(inChannel, outChannel, 1000)
-	go tls_server(inChannel, outChannel, 1000)
+	go send(outChannel, inChannel, []rune("this is a test message\neerere rererere\nhrhtrhtrhtrhtrhthr\ntjrthjeltjrekthrejthjlerkthjerltre\nqwe qew qwe qwe qwe "))
+	go receive(inChannel, outChannel, 1000)
 
 	time.Sleep(2000 * time.Millisecond)
 }
