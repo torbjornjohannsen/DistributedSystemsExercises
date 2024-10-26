97c9a870e08610e1f1cf4e0cfab221c55a0442f5
torbjornjohannsen <toej@itu.dk>
2024-10-26 17:29:48 +0200

    mandatory 3 v1

---
+++ mandatory3/client/client.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"context"
+	pb "dissys/mandatory3/Chitt_chat"
+	"flag"
+	"log"
+	"math/rand/v2"
+	"sync"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// yoinked from the example
+var (
+	tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	caFile             = flag.String("ca_file", "", "The file containing the CA root cert file")
+	serverAddr         = flag.String("addr", "localhost:50051", "The server address in the format of host:port")
+	serverHostOverride = flag.String("server_host_override", "x.test.example.com", "The server name used to verify the hostname returned by the TLS handshake")
+)
+
+func runChat(client pb.ChittChatClient) {
+	id := rand.Int()
+	lamportTime := int32(0)
+	var mu sync.Mutex
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()                  // run cancel after the execution of this function dies
+	stream, err := client.Chat(ctx) // contact the server, recieve a stream
+	if err != nil {
+		log.Fatalln("Client failed due to", err)
+	}
+
+	// goroutine to recieve and log messages
+	go func() {
+		for {
+			in, err := stream.Recv()
+			if err != nil {
+				log.Fatalln("Client failed due to ", err)
+			}
+
+			mu.Lock()
+			lamportTime = max(lamportTime+1, in.LamportTime)
+			mu.Unlock()
+
+			log.Println("Client recieved: ", in.Text, " @ ", in.LamportTime)
+		}
+	}()
+
+	numMessages := rand.IntN(50)
+	for i := 0; i < numMessages; i++ {
+		mu.Lock()
+		lamportTime++
+		mu.Unlock()
+
+		msg := pb.Message{
+			Text:        "Client ",
+			LamportTime: lamportTime,
+			SenderId:    int32(id),
+			LastMessage: i-1 == numMessages}
+
+		err := stream.Send(&msg)
+		if err != nil {
+			log.Fatalln("Client failed due to ", err)
+		}
+		time.Sleep(time.Duration(rand.IntN(2000)) * time.Millisecond)
+	}
+	stream.CloseSend()
+}
+
+func main() {
+	flag.Parse()
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	conn, err := grpc.NewClient(*serverAddr, opts...)
+	if err != nil {
+		log.Fatalln("Client failed to open connection: ", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewChittChatClient(conn)
+
+	runChat(client)
+}
---
+++ mandatory3/server/server.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	pb "dissys/mandatory3/Chitt_chat"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+
+	"google.golang.org/grpc"
+)
+
+// stolen from example
+var (
+	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile   = flag.String("cert_file", "", "The TLS cert file")
+	keyFile    = flag.String("key_file", "", "The TLS key file")
+	jsonDBFile = flag.String("json_db_file", "", "A json file containing a list of features")
+	port       = flag.Int("port", 50051, "The server port")
+)
+
+type ChatServer struct {
+	pb.UnimplementedChittChatServer
+
+	mu             sync.Mutex
+	lamportTime    int32
+	msgToBroadcast chan *pb.Message
+	channels       []chan *pb.Message
+}
+
+func (s *ChatServer) Chat(stream grpc.BidiStreamingServer[pb.Message, pb.Message]) error {
+	broadcastChan := make(chan *pb.Message)
+	s.mu.Lock()
+
+	log.Println("New connection established")
+	s.channels = append(s.channels, broadcastChan)
+
+	if len(s.channels) <= 1 { // first time we get a connection launch the broadcaster thread
+		go s.Broadcaster()
+	}
+
+	s.mu.Unlock()
+
+	// goroutine to broadcast to this particular client
+	go func() {
+		for {
+			msg := <-broadcastChan
+
+			stream.Send(msg)
+		}
+	}()
+
+	for {
+		in, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+
+		s.mu.Lock()
+		s.lamportTime = max(s.lamportTime+1, in.LamportTime)
+		s.mu.Unlock()
+
+		s.msgToBroadcast <- &pb.Message{
+			Text:        in.Text,
+			LamportTime: s.lamportTime,
+			SenderId:    in.SenderId,
+			LastMessage: in.LastMessage}
+
+		if in.LastMessage { // graceful exit
+			return nil
+		}
+	}
+}
+
+func (s *ChatServer) Broadcaster() {
+	log.Println("Broadcaster launched")
+
+	for { // just relay every message we get to all the comms we have going on
+		msg := <-s.msgToBroadcast
+		for _, channel := range s.channels {
+			channel <- msg
+		}
+	}
+}
+
+func newServer(broadcastChan chan *pb.Message) *ChatServer {
+	return &ChatServer{
+		lamportTime:    0,
+		msgToBroadcast: broadcastChan,
+		channels:       make([]chan *pb.Message, 0)}
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	broadcastChan := make(chan *pb.Message)
+	grpcServer := grpc.NewServer()
+	pb.RegisterChittChatServer(grpcServer, newServer(broadcastChan))
+	grpcServer.Serve(lis)
+
+}
